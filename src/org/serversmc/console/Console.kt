89ca256de6546b9ae668695289a60a07e663d60b
Dennis Lysenko <dennislysenko@hotmail.com>
2020-02-17 09:56:17 -0500

    Added ConsoleAPI

---
+++ src/org/serversmc/console/Console.kt
@@ -0,0 +1,31 @@
+package org.serversmc.console
+
+import org.bukkit.*
+import org.bukkit.Color.*
+import org.bukkit.plugin.*
+
+object Console {
+	
+	private lateinit var plugin: Plugin
+	
+	fun init(plugin: Plugin) {
+		this.plugin = plugin
+	}
+	
+	val consoleSender = Bukkit.getConsoleSender()
+	private const val p = "[AutoRestart] "
+	
+	fun info(s: String) = Bukkit.getConsoleSender().sendMessage("${GREEN}$p$s")
+	fun warn(s: String) = Bukkit.getConsoleSender().sendMessage("$YELLOW$p$s")
+	fun err(s: String) = Bukkit.getConsoleSender().sendMessage("$RED$p$s")
+	fun consoleSendMessage(s: String) = consoleSender.sendMessage(ChatColor.translateAlternateColorCodes('&', s))
+	
+	fun catchError(e: Exception, loc: String) {
+		err("There was an error in $loc")
+		consoleSendMessage(e.toString())
+		println(plugin.javaClass.name)
+		e.stackTrace.forEach { if (it.toString().startsWith(plugin.javaClass.name)) consoleSendMessage("\t" + it.toString()) }
+		err("End of error")
+	}
+	
+}
